015a9b8bf118b9a9edcae82fd3d788552b530f9f
Karthickkoppaka <karthickkoppaka@gmail.com>
2024-02-26 06:50:48 +0000

    deliveroo log

--- src/controller/deliveroo.events.controller.ts
+++ src/controller/deliveroo.events.controller.ts
@@ -6,9 +6,9 @@ export default async function orderEventsHandler(
     request: FastifyRequest,
     reply: FastifyReply
 ){
-    // console.log('request data', request);
-    // console.log('reply data', reply);
-    // logger.info(request, 'orderEventsHandler request data');
-    // logger.info(reply, 'orderEventsHandler reply data');
+    console.log('request data', request);
+    console.log('reply data', reply);
+    logger.info(request, 'orderEventsHandler request data');
+    logger.info(reply, 'orderEventsHandler reply data');
     return reply.code(200).send('deliveroo orderEventsHandler request received');
 }
